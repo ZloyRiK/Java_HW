83de06b0fcdf238d2175ddeabe5ea384b929f7c6
ZloRiK <r.zrk@yandex.ru>
2023-03-06 21:41:17 +0500

    Task 3 is completed

--- Home_work_1/Task_3.java
+++ Home_work_1/Task_3.java
@@ -3,39 +3,43 @@
 // Ввод знака ->
 // Ввод числа ->
 
-
 package Home_work_1;
 
 import java.util.Scanner;
 
 public class Task_3 {
     public static void main(String[] args) {
-        int val_1=0, val_2=0;
-        var res =0;
-        String simbol="";
+        int val_1 = 0, val_2 = 0;
+        var res = 0;
+        char simbol;
         Scanner value = new Scanner(System.in);
         System.out.print("Введите первое число ");
-        val_1=value.nextInt();
+        val_1 = value.nextInt();
         System.out.print("Введите символ ");
-        simbol=value.next();
+        simbol = value.next().charAt(0);
         System.out.print("Введите второе число ");
-        val_2=value.nextInt();
+        val_2 = value.nextInt();
         value.close();
-        if (simbol.contains("+")){
-            res = val_1+val_2;
-            System.out.printf("Ответ: %s", res);
-        }
-        if (simbol.contains("-")){
-            res = val_1-val_2;
-            System.out.printf("Ответ: %s",res);
-        }
-        if (simbol.contains("*")){
-            res = val_1*val_2;
-            System.out.printf("Ответ: %s",res);
-        }
-        if (simbol.contains("/")){
-            res = val_1/val_2;
-            System.out.printf("Ответ: %s",res);
+        switch (simbol) {
+            case '+':
+                res = val_1 + val_2;
+                System.out.printf("Ответ: %s", res);
+                break;
+            case '-':
+                res = val_1 - val_2;
+                System.out.printf("Ответ: %s", res);
+                break;
+            case '*':
+                res = val_1 * val_2;
+                System.out.printf("Ответ: %s", res);
+                break;
+            case '/':
+                res = val_1 / val_2;
+                System.out.printf("Ответ: %s", res);
+                break;
+            default:
+                System.out.println("Символ не прочитался");
+                break;
         }
     }
 }
